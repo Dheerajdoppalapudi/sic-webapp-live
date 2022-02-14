b7f9e23bae59e995c88c52e24a0c9806e2ac4131
Dheerajdoppalapudi <63925819+Dheerajdoppalapudi@users.noreply.github.com>
2022-02-15 01:00:03 +0530

    Update settings.py

--- sicwebapp/settings.py
+++ sicwebapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n!s01(ahcd2mgu!bt5hci52+d%4@=_f@&quqt02!+1m$tt$gv6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
